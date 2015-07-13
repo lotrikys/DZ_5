992a516f13036d297ef72293bd73aaaf44d0328f
root <lotrik@mail.ru>
2015-07-13 10:05:01 +0300

    Final

--- src/com/itstep/patukhov/Main.java
+++ src/com/itstep/patukhov/Main.java
@@ -10,14 +10,14 @@ package com.itstep.patukhov;
  * Created by lotrik on 12.07.15.
  */
 public class Main {
-    static int[] mas = {1,2,3,4,5,-6,7,-8,9,10};
+    static int[] mas = {1,-2,3,4,5,-6,7,8,9,10};
 
     public static void main(String[] args) {
-        for (int i=0; i<mas.length; i++) {
-            if (mas[i]<0) {
-                System.out.println(new MyException ("Отрицательный элемент: " + mas[i] + " на позиции: " + i));
-                break;
-            }
+        try {
+            String result = Mas.getMas(mas);
+            System.out.println(result);
+        } catch (MyException ex) {
+            System.out.println(ex.getMessage());
         }
     }
 }
